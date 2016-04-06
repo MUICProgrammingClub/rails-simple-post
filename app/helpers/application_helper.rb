074c204c5dbfc07fd7aabc92d285008d024b79ee
Paween Theanvanichpant <paween.the@gmail.com>
2016-04-07 00:52:29 +0700

    Finish user part

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def header_active(controller)
+    return 'active' if controller == params[:controller]
+    return ''
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,7 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
 end
